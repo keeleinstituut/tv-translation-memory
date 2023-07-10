32b4d46e96e9fe8ad5a58948670dbdec96fba400
higherstar <goldbyol@outlook.com>
2023-07-10 09:50:59 +0000

    Fix issues after docker image change

--- src/RestApi/Api.py
+++ src/RestApi/Api.py
@@ -58,8 +58,8 @@ app.logger.handlers.extend(logging.getLogger("gunicorn.error").handlers)
 principals = Principal(app)
 
 # Celery configuration
-app.config['CELERY_BROKER_URL'] = 'redis://localhost:6379/0'
-app.config['CELERY_RESULT_BACKEND'] = 'redis://localhost:6379/0'
+app.config['CELERY_BROKER_URL'] = 'redis://redis:6379/0'
+app.config['CELERY_RESULT_BACKEND'] = 'redis://redis:6379/0'
 # Initialize Celery
 celery = Celery(app.name, broker=app.config['CELERY_BROKER_URL'])
 celery.conf.update(app.config)
