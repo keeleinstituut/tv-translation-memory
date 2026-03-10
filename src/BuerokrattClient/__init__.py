574bc4126a030a7ec08b10ab797f9be24e08e192
dmytro <dmytro@fluidum.ee>
2026-03-10 13:49:09 +0200

    hotfix: disable anonymizer requests if `ANONYMISER_URL` env variable is not set

--- src/BuerokrattClient/__init__.py
+++ src/BuerokrattClient/__init__.py
@@ -8,11 +8,15 @@ from Config.Config import G_CONFIG
 
 class BuerokrattAnonymizerClient:
     def __init__(self):
-        self.baseurl = G_CONFIG.config['anonymiser']['url']
+        url = G_CONFIG.config.get('anonymiser', {}).get('url')
+        self.baseurl = url if url and url != 'ANONYMISER_URL' else None
         self.timeout = 30
         self.ssl_context = None
 
     def predict(self, texts, pseudonymize=True, tokenize=True, truecase=True):
+        if not self.baseurl:
+            return [{'sisendtekst': None, 'anonümiseeritud_tekst': None} for _ in texts]
+
         url = "{0}/predict".format(self.baseurl)
         data = {
             'texts': texts,
